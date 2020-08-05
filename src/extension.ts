e46c682de5119c7c8702f13c79621544d03afba9
Anton Shibayev <anton.shibayev@gmail.com>
2020-08-06 01:05:35 +0300

    alpha

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,52 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const showInfo = (message: string) => vscode.window.showInformationMessage(`TypeImport: ${message}`);
+	const showError = (errorNo: number, message?: string) => showInfo(`Error #${errorNo}.` + (message ? ` ${message}` : null));
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "type-import" is now active!');
+	const disposable = vscode.commands.registerCommand('type-import.import', () => {
+		const editor = vscode.window.activeTextEditor;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('type-import.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+		if (!editor) {
+			showError(1, 'No active editor found.');
+			return;
+		}
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from TypeImport!');
+		const selection = editor.selection;
+
+		const fullTypeName = editor.document.getText(selection).trim();
+
+		const parts = fullTypeName.split('.');
+		const alias = parts[parts.length - 1];
+
+		editor.edit(e => {
+			const importExpr = new RegExp(/^import.+/gim);
+			const importLine = `import ${alias} = ${fullTypeName};` + `\n`;
+
+			let insertPosition = 0;
+
+			const text = editor.document.getText();
+
+			const importMatches = text.match(importExpr);
+
+			if (importMatches?.length) {
+				const lastImportMatch = importMatches[importMatches.length - 1];
+
+				const lastImportPosition = text.lastIndexOf(lastImportMatch);
+
+				insertPosition = text.indexOf(';', lastImportPosition);
+			}
+
+			const position = insertPosition === 0
+				? new vscode.Position(0, 0)
+				: editor.document.positionAt(insertPosition).translate(1, 0);
+
+			e.insert(position, importLine);
+			e.replace(selection, alias);
+		});
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}

3da3c717cbf0cc4066f036517c4c7e854b62332e
Anton Shibayev <anton.shibayev@gmail.com>
2020-08-15 20:11:18 +0300

    fix duplicate imports

--- src/extension.ts
+++ src/extension.ts
@@ -7,57 +7,57 @@ export function activate(context: vscode.ExtensionContext) {
 	try {
 		const disposable = vscode.commands.registerCommand('type-import.import', () => {
 			const editor = vscode.window.activeTextEditor;
-	
+
 			if (!editor) {
 				showError(1, 'No active editor found.');
 				return;
 			}
-	
+
 			const selection = editor.selection;
 			const selectionText = editor.document.getText(selection);
-	
+
 			const fullTypeName = selectionText.trim();
-	
+
 			if (!fullTypeName) {
 				showError(2, 'Nothing to import.');
 				return;
 			}
-	
+
 			if (fullTypeName.indexOf('.') === -1) {
 				showInfo('No import required.');
 				return;
 			}
-	
+
 			const parts = fullTypeName.split('.');
 	
 			if (parts.some(x => !x || x.includes(' '))) {
 				showError(3, 'Invalid type.');
 				return;
 			}
-	
+
 			const alias = parts[parts.length - 1];
-	
+
 			editor.edit(e => {
-				const importLine = `import ${alias} = ${fullTypeName};`;
-	
-				let insertPosition = 0;
-	
 				const documentContent = editor.document.getText();
-	
+
+				const importLine = `import ${alias} = ${fullTypeName};`;
+
+				let insertPosition = 0;
+
 				const importMatches = documentContent.match(/^import.+/gim);
-	
+
 				if (importMatches?.length) {
 					const lastImportMatch = importMatches[importMatches.length - 1];
-	
+
 					const lastImportPosition = documentContent.lastIndexOf(lastImportMatch);
-	
+
 					insertPosition = documentContent.indexOf(';', lastImportPosition);
 				}
-	
+
 				const position = insertPosition === 0
 					? new vscode.Position(0, 0)
 					: editor.document.positionAt(insertPosition).translate(1, 0);
-	
+
 				const beginningSpaceCount = selectionText.search(/\S/);
 
 				const endSpaceIndex = selectionText.search(/\s+$/m);
@@ -78,7 +78,7 @@ export function activate(context: vscode.ExtensionContext) {
 				e.replace(replaceSelection, alias);
 			});
 		});
-	
+
 		context.subscriptions.push(disposable);
 	} catch {
 		showError(0, 'Unknown error.');
